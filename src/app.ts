9a20915d0f69a02b2e8a58a4ed2f13cf542fab13
Mahir4098 <mahirjr98@gmail.com>
2026-05-26 20:26:38 +0600

    Ready for Deploy

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use("/api/auth/", authRoute);
 app.use("/api/issues", issueRoute);
 
 app.get("/", async (req: Request, res: Response) => {
-  res.send("Hello World");
+  res.send("Dev Pulse Server Is Running...");
 });
 
 export default app;
